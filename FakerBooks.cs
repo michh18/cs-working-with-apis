ce548afb64177190325a2c54785df31de73ed637
michh18 <michellechan0518@gmail.com>
2026-04-22 14:39:51 +0100

    feat: Initial commit

---
+++ FakerBooks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace cs_working_with_apis
+{
+    internal class FakerBooks
+    {
+        private static readonly string BASE_URL = "https://fakerapi.it/api/v1/books";
+
+        public static async Task<FakerBookResponse?> GetBooksAsync(int quantity)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = $"{BASE_URL}?_quantity={quantity}";
+
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrEmpty(responseBody))
+                    {
+                        return JsonSerializer.Deserialize<FakerBookResponse>(responseBody);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not be deserialized");
+                        return null;
+                    }
+                    //Console.WriteLine(responseBody);
+
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error:{e.Message}");
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace cs_working_with_apis
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            FakerBookResponse? res = await FakerBooks.GetBooksAsync(2);
+            if (res != null)
+            {
+                Console.WriteLine(res.Data[0].Id);
+            }
+        }
+    }
+}
